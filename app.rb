83ff16eef66db24616478bb4dbab0cb92018e8ee
stellarwobble <jennidemuir@gmail.com>
2020-05-11 14:45:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is jenni"
+
+    end
+
+    get '/hometown' do
+        "My hometown is San Jose"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Debaser"
+    end
+
 end
